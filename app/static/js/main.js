da83653c539b296e16cdea6b9ab1cb9b18fb4444
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-04-28 21:28:38 +1000

    made the linter stp complaining

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,11 +1,10 @@
-$(document).ready(function() {
-  // alert("hello")
-  let height = $(document).height()
-  $(window).scroll(function() {
+$( document ).ready( function() {
+	var height = $( document ).height();
+	$( window ).scroll( function() {
 
-    console.log(parseInt(($(this).scrollTop()/height)*100));
-    if($(this).scroll()) {
-      let initial = $(this).scroll()
-    }
-  })
-})
+		console.log( parseInt( ( $( this ).scrollTop() / height ) * 100 ) );
+		if ( $( this ).scroll() ) {
+			let initial = $( this ).scroll();
+		}
+	} );
+} );
