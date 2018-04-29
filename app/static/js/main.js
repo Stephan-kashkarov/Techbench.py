cbcf312c4eb20a82ba25f9d7d2194c31a536e16c
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-04-29 10:52:30 +1000

    many issues

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,10 +1,9 @@
 $( document ).ready( function() {
 	var height = $( document ).height();
 	$( window ).scroll( function() {
-
 		console.log( parseInt( ( $( this ).scrollTop() / height ) * 100 ) );
 		if ( $( this ).scroll() ) {
-			let initial = $( this ).scroll();
+			var initial = $( this ).scroll();
 		}
 	} );
 } );
